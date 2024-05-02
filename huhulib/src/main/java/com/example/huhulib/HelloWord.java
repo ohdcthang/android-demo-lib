1a18308ce5987ca0421c19710cb060465e048302
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-05-02 11:04:26 +0700

    init

---
+++ huhulib/src/main/java/com/example/huhulib/HelloWord.java
@@ -0,0 +1,13 @@
+package com.example.huhulib;
+
+import android.util.Log;
+
+public class HelloWord {
+    public static  void  main(String[] args){
+        //Main method
+    }
+
+    public static  void printLog(String tag, String msg){
+        Log.d(tag, msg);
+    }
+}
